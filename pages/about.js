fd05eca7ef9b9b2fc8575cd6bc4a3604b315cb61
Eduardo Miano <eduardo.miano@btgpactual.com>
2022-02-15 16:58:32 -0300

    primeiras telas

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import Link from 'next/link'
+
+function About() {
+    return (<Link href='/'><a><img src='/images/BG_Teams.jpg' alt='BTG Pactual' /></a></Link>)
+}
+
+export default About
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,52 @@
-function Home(){
-    return <div>HOME Miano Segundo commit</div>
+import { useState } from 'react'
+import Link from 'next/link'
+import Layout from '../components/layout'
+
+function Index() {
+
+    return (
+        <>
+            <div>Ola mundo</div>
+            <Contador />
+            <Link href='/about'>
+                <a>About</a>
+            </Link>
+        </>
+    )
 }
 
-export default Home;
+function Contador() {
+    const [contador, setContador] = useState(0)
+
+    function addCount() {
+        contador += 1;
+        setContador(contador);
+    }
+
+    function removeCount() {
+        if (contador > 0)
+            contador -= 1
+
+        setContador(contador)
+    }
+
+    debugger
+
+    return (
+        <>
+            <div>Contador: {contador}</div>
+            <button onClick={addCount}>Add</button>
+            <button onClick={removeCount}>Remove</button>
+        </>
+    )
+}
+
+Index.getLayout = function getLayout(page) {
+  return (
+    <Layout>
+      {page}
+    </Layout>
+  )
+}
+
+export default Index;
