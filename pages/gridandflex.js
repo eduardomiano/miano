fe5cbbe1aa5174b877a05062af64e914aa2974d6
Eduardo Miano <eduardo.miano@btgpactual.com>
2022-02-21 16:09:36 -0300

    urls roadmunk

---
+++ pages/gridandflex.js
@@ -0,0 +1,10 @@
+const Gridandflex = (props) => {
+    return (
+        <div className="bg-blue-100">
+            <div className="bg-red-50">1</div>
+            <div className="bg-yellow-50">2</div>
+        </div>
+    )
+}
+
+export default Gridandflex;
--- pages/index.js
+++ pages/index.js
@@ -22,17 +22,17 @@ function Index() {
                 <div className="mt-10">
                     <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
                         {features.map((feature) => (
-                            <Link href='/roadmap/[...slug]' as='/roadmap/046b53547f74e473be48014d1628c12f8dfe5424'>
-                                <a>
-                                <div key={feature.name} className="relative">
-                                    <dt>
-                                        <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                                            <feature.icon className="h-6 w-6" aria-hidden="true" />
-                                        </div>
-                                        <p className="ml-16 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
-                                    </dt>
-                                    <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
-                                </div>
+                            <Link href='/roadmap/[...slug]' as={feature.url}>
+                                <a className='hover:bg-indigo-50'>
+                                    <div key={feature.name} className="relative">
+                                        <dt>
+                                            <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                                                <feature.icon className="h-6 w-6" aria-hidden="true" />
+                                            </div>
+                                            <p className="ml-16 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
+                                        </dt>
+                                        <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
+                                    </div>
                                 </a>
                             </Link>
                         ))}
@@ -86,60 +86,70 @@ const features = [
         description:
             'Conta corrente, Saldo Remunerado, Cheque Especial, Boletos, etc',
         icon: GlobeAltIcon,
+        url: 'roadmap/4de8f2cce7208f48616d18089a7448b7fe79224a'
     },
     {
         name: 'Risco Sacado',
         description:
             'Quickfin e Integrações de Sacados.',
         icon: ScaleIcon,
+        url: 'roadmap/046b53547f74e473be48014d1628c12f8dfe5424'
     },
     {
         name: 'BTG ID',
-        description:
-            'BTG ID e Onboarding PJ Unificado',
+        description: 'BTG ID e Onboarding PJ Unificado',
         icon: LightningBoltIcon,
+        url: 'roadmap/78f7223ac5ef0cfc25de5eb1519586cbbb5ea1a3'
+
     },
     {
         name: 'Mobile',
         description:
             'Aplicativo Mobile.',
         icon: AnnotationIcon,
+        url: 'roadmap/3f36db78a0c3df6bf848aba6544a6a743f349f5d'
     },
     {
         name: 'Billing Fee',
         description:
             'Billing Fee & Rebate.',
         icon: LightningBoltIcon,
+        url: 'roadmap/464457f8b5f54c7454ae1b575151d807a8094e34'
     },
     {
         name: 'Credito',
         description:
             'Capital de Gito, Emprestimos, Agro e Solar.',
         icon: AnnotationIcon,
+        url: 'roadmap/78f7223ac5ef0cfc25de5eb1519586cbbb5ea1a3'
     },
     {
         name: 'Developers',
         description:
             'Automações, Catalogo de APIs.',
         icon: LightningBoltIcon,
+        url: 'roadmap/78f7223ac5ef0cfc25de5eb1519586cbbb5ea1a3'
     },
     {
         name: 'Cartões',
         description:
             'Cartão de Crédtio e Débito.',
         icon: AnnotationIcon,
+        url: 'roadmap/78f7223ac5ef0cfc25de5eb1519586cbbb5ea1a3'
     },
     {
         name: 'LATAM',
         description:
             'Chile e Colombia.',
         icon: AnnotationIcon,
+        url: 'roadmap/78f7223ac5ef0cfc25de5eb1519586cbbb5ea1a3'
     },
     {
         name: 'Atendimento',
         description:
             'Portal Admin e Portal de Antedimento.',
         icon: AnnotationIcon,
+        url: 'roadmap/78f7223ac5ef0cfc25de5eb1519586cbbb5ea1a3'
     },
 ]
 
