988534cc5aae8009e7bf7e717821b31a47f7c1fa
Eduardo Miano <eduardo.miano@btgpactual.com>
2022-02-22 02:36:16 -0300

    getinitialprops sample

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,10 @@
 import Layout from '../components/layout'
 
 
-function About() {
+ function About({ repos }) {
+
+    console.log(repos)
+
     return (
 
         <>
@@ -10,22 +13,24 @@ function About() {
                 Toggle modal
             </button>
 
-            <div id="authentication-modal" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center h-modal md:h-full md:inset-0">
-                <div className="relative px-4 w-full max-w-md h-full md:h-auto">
-
-                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                        <div className="flex justify-end p-2">
-                            <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="authentication-modal">
-                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path></svg>
-                            </button>
-                        </div>
-                       miano
-                    </div>
-                </div>
-            </div>
+            
+            {repos.map((r) => 
+                <div key={r.id}>{r.name}</div>
+            )}
         </>
-
     )
+}
+
+About.getInitialProps = async (ctx) => {
+    try {
+        const res = await fetch('https://api.github.com/users/eduardomiano/repos')
+        const result = await res.json()
+        console.log(result)
+        return { repos: result }
+    } catch (error) {
+        console.log('error:')
+        console.error(error)
+    }
 }
 
 About.getLayout = function getLayout(page) {
